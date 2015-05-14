054fb779131c85e8af9de98b7c08f583d41c2912
Hakan Özler <ozler.hakan@gmail.com>
2015-05-14 19:41:29 +0300

    Update TweetEntity class

--- spring-data-examples/src/java/com/kodcu/mongodb/spring/twitter/db/TweetEntity.java
+++ spring-data-examples/src/java/com/kodcu/mongodb/spring/twitter/db/TweetEntity.java
@@ -9,7 +9,7 @@ import java.util.List;
  */
 class TweetEntity {
 
-    private List<MediaEntity> media = new ArrayList<>();
+    private List<MediaProperty> media = new ArrayList<>();
     private List<URLEntity> urls = new ArrayList<>();
     private List<HashTagEntity> hashtags = new ArrayList<>();
     private List<MentionEntity> user_mentions = new ArrayList<>();
@@ -19,7 +19,7 @@ class TweetEntity {
         return hashtags;
     }
 
-    public List<MediaEntity> getMedia() {
+    public List<MediaProperty> getMedia() {
         return media;
     }
 
@@ -62,7 +62,7 @@ class TweetEntity {
     }
 
     class MentionEntity {
-        private int id;
+        private Integer id;
         private String id_str;
         private String screen_name;
         private String name;
@@ -78,118 +78,6 @@ class TweetEntity {
                     ", indices=" + Arrays.toString(indices) +
                     '}';
         }
-    }
-
-    class MediaEntity {
-
-        private Long id;
-        private MediaProperty.Size sizes;
-        private int[] indices;
-
-        private String url;
-        private String type;
-        private String id_str;
-        private String media_url;
-        private String display_url;
-        private String expanded_url;
-        private String media_url_https;
-
-        public String getDisplay_url() {
-            return display_url;
-        }
-
-        public void setDisplay_url(String display_url) {
-            this.display_url = display_url;
-        }
-
-        public String getExpanded_url() {
-            return expanded_url;
-        }
-
-        public void setExpanded_url(String expanded_url) {
-            this.expanded_url = expanded_url;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public void setId(long id) {
-            this.id = id;
-        }
-
-        public String getId_str() {
-            return id_str;
-        }
-
-        public void setId_str(String id_str) {
-            this.id_str = id_str;
-        }
-
-        public int[] getIndices() {
-            return indices;
-        }
-
-        public void setIndices(int[] indices) {
-            this.indices = indices;
-        }
-
-        public String getMedia_url() {
-            return media_url;
-        }
-
-        public void setMedia_url(String media_url) {
-            this.media_url = media_url;
-        }
-
-        public String getMedia_url_https() {
-            return media_url_https;
-        }
-
-        public void setMedia_url_https(String media_url_https) {
-            this.media_url_https = media_url_https;
-        }
-
-        public MediaProperty.Size getSizes() {
-            return sizes;
-        }
-
-        public void setSizes(MediaProperty.Size sizes) {
-            this.sizes = sizes;
-        }
-
-        public String getType() {
-            return type;
-        }
-
-        public void setType(String type) {
-            this.type = type;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        @Override
-        public String toString() {
-            return "MediaEntity{" +
-                    "display_url='" + display_url + '\'' +
-                    ", media_url='" + media_url + '\'' +
-                    ", media_url_https='" + media_url_https + '\'' +
-                    ", type='" + type + '\'' +
-                    ", sizes=" + sizes +
-                    ", id=" + id +
-                    ", id_str='" + id_str + '\'' +
-                    ", indices=" + Arrays.toString(indices) +
-                    ", url='" + url + '\'' +
-                    ", expanded_url='" + expanded_url + '\'' +
-                    '}';
-        }
-
     }
 
     @Override
